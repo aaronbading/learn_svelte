e97c9ccab4951a2c3e75e2f6992f1a51cd92bfe0
Aaron Bading <aaronbading.ab@gmail.com>
2023-05-01 18:34:21 -0500

    continuing the tutorial

--- svelte_tutorial/src/routes/+page.svelte
+++ svelte_tutorial/src/routes/+page.svelte
@@ -4,6 +4,8 @@
     let name = 'world';
     let src = 'https://media.giphy.com/media/3o7aD2jBmUohFAOZ5W/giphy.gif';
     // let src = '../../static/favicon.png';  // dont know why this does not work .. 
+
+    
 </script>
 
 <style>
@@ -17,5 +19,6 @@
 <h1>Hello {name}!</h1>
 <img {src} alt="A man dances.">
 <Nested />
+
 <Button/>
-<p>Added style</p>
+
--- svelte_tutorial/src/routes/Button.svelte
+++ svelte_tutorial/src/routes/Button.svelte
@@ -1,5 +1,16 @@
 <script>
 	let count = 0;
+	$: doubled = count * 2;
+//     $: {
+// 	console.log('the count is ' + count);
+// 	alert('I SAID THE COUNT IS ' + count);
+// }
+
+    $: if (count >= 10) {
+        alert('count is dangerously high!');
+        count = 9;
+    }
+    $: console.log('the count is ' + count);
 
 	function incrementCount() {
 		count += 1;
@@ -9,3 +20,4 @@
 <button on:click={incrementCount}>
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
+<p>{count} doubled is {doubled}</p>
