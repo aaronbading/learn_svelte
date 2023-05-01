125de1feb967101d383b69322b45ef29b6947934
Aaron Bading <aaronbading.ab@gmail.com>
2023-05-01 18:43:17 -0500

    spread props

--- svelte_tutorial/src/routes/+page.svelte
+++ svelte_tutorial/src/routes/+page.svelte
@@ -2,6 +2,16 @@
 	import Nested from './Nested.svelte';
     import Button from './Button.svelte';
     import AddNumberButton from './AddNumberButton.svelte';
+    import Info from './Info.svelte';
+
+    const pkg = {
+		name: 'svelte',
+		version: 3,
+		speed: 'blazing',
+		website: 'https://svelte.dev'
+	};
+
+
     let name = 'world';
     let src = 'https://media.giphy.com/media/3o7aD2jBmUohFAOZ5W/giphy.gif';
     // let src = '../../static/favicon.png';  // dont know why this does not work .. 
@@ -24,4 +34,8 @@
 
 <Button/>
 <AddNumberButton/>
+
+
+<!-- <Info name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website}/>  this is equivalent to the below line -->
+<Info {...pkg}/>
 
---
+++ svelte_tutorial/src/routes/Info.svelte
@@ -0,0 +1,28 @@
+<script>
+	/**
+	 * @type {any}
+	 */
+	 export let name;
+	/**
+	 * @type {any}
+	 */
+	 export let version;
+
+     
+	/**
+	 * @type {any}
+	 */
+	 export let speed;
+     
+	/**
+	 * @type {any}
+	 */
+	 export let website;
+
+</script>
+
+<p>
+	The <code>{name}</code> package is {speed} fast.
+	Download version {version} from <a href="https://www.npmjs.com/package/{name}">npm</a>
+	and <a href={website}>learn more here</a>
+</p>
